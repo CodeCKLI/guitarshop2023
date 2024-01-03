ec5ac45ebefaa72c2b2d68c0908526b82f086aa6
Mattzac <hk.mattli@gmail.com>
2024-01-03 12:52:05 -0500

    delpoy1

--- guitarshop_api/src/main/java/com/ckdev/guitarshop_api/GuitarshopApiApplication.java
+++ guitarshop_api/src/main/java/com/ckdev/guitarshop_api/GuitarshopApiApplication.java
@@ -43,92 +43,92 @@ public class GuitarshopApiApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		GuitarEntity guitarA = GuitarEntity.builder()
-				.model("70TH ANNIVERSARY PLAYER STRATOCASTER®")
-				.brand("Fender")
-				.description("Celebrate 70 years of Fender Stratocaster excellence with the 70th Anniversary Collection. From vintage charm to modern innovation, each model offers iconic style, rich tones, and limited availability.")
-				.cover_URL("src/assets/guitarpics/fender70strat.jpg")
-				.price(799.99)
-				.colorOptions(Arrays.asList("black"))
-				.pictures(Arrays.asList("src/assets/guitarpics/fender70strats1.jpg",
-						"src/assets/guitarpics/fender70strats2.jpg",
-						"src/assets/guitarpics/fender70strats3.jpg"))
-				.bodyColor("black")
-				.build();
-
-		GuitarEntity guitarB = GuitarEntity.builder()
-				.model("SG Classic Worn P-90s")
-				.brand("Epiphone")
-				.description("Epiphone's SG Classic Worn, part of the Inspired by Gibson™ Collection, features a  finish, P-90 PRO™ pickups, and comes in Worn Cherry and Worn Inverness Green.")
-				.cover_URL("src/assets/guitarpics/epiphonesgred.png")
-				.price(429.99)
-				.colorOptions(Arrays.asList("red", "green"))
-				.pictures(Arrays.asList("src/assets/guitarpics/epiphonesgred1.png",
-						"src/assets/guitarpics/epiphonesgred2.png",
-						"src/assets/guitarpics/epiphonesgred3.png"))
-				.bodyColor("red")
-				.build();
-
-		GuitarEntity guitarD = GuitarEntity.builder()
-				.model("SG Classic Worn P-90s")
-				.brand("Epiphone")
-				.description("Epiphone's SG Classic Worn, part of the Inspired by Gibson™ Collection, features a  finish, P-90 PRO™ pickups, and comes in Worn Cherry and Worn Inverness Green.")
-				.cover_URL("src/assets/guitarpics/epiphonesggreen.png")
-				.price(429.99)
-				.colorOptions(Arrays.asList("red", "green"))
-				.pictures(Arrays.asList("src/assets/guitarpics/epiphonesggreen1.png",
-						"src/assets/guitarpics/epiphonesggreen2.png",
-						"src/assets/guitarpics/epiphonesggreen3.png"))
-				.bodyColor("green")
-				.build();
-
-		GuitarEntity guitarC = GuitarEntity.builder()
-				.model("RG8870")
-				.brand("Ibanez")
-				.description("Ibanez guitar boasts Super Wizard AS neck, j.custom design, Macassar Ebony fretboard, chambered body, DiMarzio pickups, Lo-Pro Edge tremolo, and versatile features.")
-				.cover_URL("src/assets/guitarpics/ibanezrg.png")
-				.price(1299.79)
-				.colorOptions(Arrays.asList("grey"))
-				.pictures(Arrays.asList("src/assets/guitarpics/ibanezrg1.png",
-						"src/assets/guitarpics/ibanezrg2.png",
-						"src/assets/guitarpics/ibanezrg3.png"))
-				.bodyColor("grey")
-				.build();
-
-		AppUserEntity appuserOne = AppUserEntity.builder()
-				.email("matt@matt.com")
-				.firstname("Matt")
-				.lastname("Li")
-				.password("$2a$10$PbgBAHBchabk0Z3Rkib7XOI8c2ELKaiJ6EqIpHWkvaKa1VREqmFyW")
-				.build();
-
-		OrderEntity orderOne = OrderEntity.builder()
-				.customerEmail("testing@email.com")
-				.customerFirstName("TestingFirst")
-				.customerLastName("TestingLast")
-				.customerPhone("6661234321")
-				.customerCountry("USA")
-				.customerAddress("123 ABC Dr")
-				.customerCity("NYC")
-				.customerPostal("123321")
-				.paymentMethod("visa")
-				.cardNumber("4242 4242 4242 4242")
-				.cardCSV("242")
-				.appuser(null)
-				.status("Penning")
-				.guitarList("[{\"id\":2,\"model\":\"SG Classic Worn P-90s\",\"bodyColor\":\"red\",\"amount\":4,\"price\":429.99,\"coverURL\":\"src/assets/guitarpics/epiphonesgred.png\",\"brand\":\"Epiphone\"},{\"id\":4,\"model\":\"SG Classic Worn P-90s\",\"bodyColor\":\"green\",\"amount\":4,\"price\":429.99,\"coverURL\":\"src/assets/guitarpics/epiphonesggreen.png\",\"brand\":\"Epiphone\"},{\"id\":3,\"model\":\"RG8870\",\"bodyColor\":\"grey\",\"amount\":1,\"price\":1299.79,\"coverURL\":\"src/assets/guitarpics/ibanezrg.png\",\"brand\":\"Ibanez\"}]")
-				.tax(123.00)
-				.shipping(5.00)
-				.total(1234.00)
-				.build();
-
-
-		guitarRepoObj.save(guitarA);
-		guitarRepoObj.save(guitarB);
-		guitarRepoObj.save(guitarC);
-		guitarRepoObj.save(guitarD);
-
-		appuserRepoObj.save(appuserOne);
+//		GuitarEntity guitarA = GuitarEntity.builder()
+//				.model("70TH ANNIVERSARY PLAYER STRATOCASTER®")
+//				.brand("Fender")
+//				.description("Celebrate 70 years of Fender Stratocaster excellence with the 70th Anniversary Collection. From vintage charm to modern innovation, each model offers iconic style, rich tones, and limited availability.")
+//				.cover_URL("src/assets/guitarpics/fender70strat.jpg")
+//				.price(799.99)
+//				.colorOptions(Arrays.asList("black"))
+//				.pictures(Arrays.asList("src/assets/guitarpics/fender70strats1.jpg",
+//						"src/assets/guitarpics/fender70strats2.jpg",
+//						"src/assets/guitarpics/fender70strats3.jpg"))
+//				.bodyColor("black")
+//				.build();
+//
+//		GuitarEntity guitarB = GuitarEntity.builder()
+//				.model("SG Classic Worn P-90s")
+//				.brand("Epiphone")
+//				.description("Epiphone's SG Classic Worn, part of the Inspired by Gibson™ Collection, features a  finish, P-90 PRO™ pickups, and comes in Worn Cherry and Worn Inverness Green.")
+//				.cover_URL("src/assets/guitarpics/epiphonesgred.png")
+//				.price(429.99)
+//				.colorOptions(Arrays.asList("red", "green"))
+//				.pictures(Arrays.asList("src/assets/guitarpics/epiphonesgred1.png",
+//						"src/assets/guitarpics/epiphonesgred2.png",
+//						"src/assets/guitarpics/epiphonesgred3.png"))
+//				.bodyColor("red")
+//				.build();
+//
+//		GuitarEntity guitarD = GuitarEntity.builder()
+//				.model("SG Classic Worn P-90s")
+//				.brand("Epiphone")
+//				.description("Epiphone's SG Classic Worn, part of the Inspired by Gibson™ Collection, features a  finish, P-90 PRO™ pickups, and comes in Worn Cherry and Worn Inverness Green.")
+//				.cover_URL("src/assets/guitarpics/epiphonesggreen.png")
+//				.price(429.99)
+//				.colorOptions(Arrays.asList("red", "green"))
+//				.pictures(Arrays.asList("src/assets/guitarpics/epiphonesggreen1.png",
+//						"src/assets/guitarpics/epiphonesggreen2.png",
+//						"src/assets/guitarpics/epiphonesggreen3.png"))
+//				.bodyColor("green")
+//				.build();
+//
+//		GuitarEntity guitarC = GuitarEntity.builder()
+//				.model("RG8870")
+//				.brand("Ibanez")
+//				.description("Ibanez guitar boasts Super Wizard AS neck, j.custom design, Macassar Ebony fretboard, chambered body, DiMarzio pickups, Lo-Pro Edge tremolo, and versatile features.")
+//				.cover_URL("src/assets/guitarpics/ibanezrg.png")
+//				.price(1299.79)
+//				.colorOptions(Arrays.asList("grey"))
+//				.pictures(Arrays.asList("src/assets/guitarpics/ibanezrg1.png",
+//						"src/assets/guitarpics/ibanezrg2.png",
+//						"src/assets/guitarpics/ibanezrg3.png"))
+//				.bodyColor("grey")
+//				.build();
+//
+//		AppUserEntity appuserOne = AppUserEntity.builder()
+//				.email("matt@matt.com")
+//				.firstname("Matt")
+//				.lastname("Li")
+//				.password("$2a$10$PbgBAHBchabk0Z3Rkib7XOI8c2ELKaiJ6EqIpHWkvaKa1VREqmFyW")
+//				.build();
+//
+//		OrderEntity orderOne = OrderEntity.builder()
+//				.customerEmail("testing@email.com")
+//				.customerFirstName("TestingFirst")
+//				.customerLastName("TestingLast")
+//				.customerPhone("6661234321")
+//				.customerCountry("USA")
+//				.customerAddress("123 ABC Dr")
+//				.customerCity("NYC")
+//				.customerPostal("123321")
+//				.paymentMethod("visa")
+//				.cardNumber("4242 4242 4242 4242")
+//				.cardCSV("242")
+//				.appuser(null)
+//				.status("Penning")
+//				.guitarList("[{\"id\":2,\"model\":\"SG Classic Worn P-90s\",\"bodyColor\":\"red\",\"amount\":4,\"price\":429.99,\"coverURL\":\"src/assets/guitarpics/epiphonesgred.png\",\"brand\":\"Epiphone\"},{\"id\":4,\"model\":\"SG Classic Worn P-90s\",\"bodyColor\":\"green\",\"amount\":4,\"price\":429.99,\"coverURL\":\"src/assets/guitarpics/epiphonesggreen.png\",\"brand\":\"Epiphone\"},{\"id\":3,\"model\":\"RG8870\",\"bodyColor\":\"grey\",\"amount\":1,\"price\":1299.79,\"coverURL\":\"src/assets/guitarpics/ibanezrg.png\",\"brand\":\"Ibanez\"}]")
+//				.tax(123.00)
+//				.shipping(5.00)
+//				.total(1234.00)
+//				.build();
+//
+//
+//		guitarRepoObj.save(guitarA);
+//		guitarRepoObj.save(guitarB);
+//		guitarRepoObj.save(guitarC);
+//		guitarRepoObj.save(guitarD);
+//
+//		appuserRepoObj.save(appuserOne);
 
 //		orderRepoObj.save(orderOne);
 
